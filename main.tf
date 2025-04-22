6d30678729fd6e8c15d49547a303e4a5710400d7
dan-demos <danpeacock96+new@gmail.com>
2025-04-22 09:43:28 +0100

    Update Name

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-039af3bfc52681cd5"
   instance_type = "t2.micro"
   tags = {
-    Name = "Learn-HCP-Packer"
+    Name = "ubuntu-packer-vm"
   }
 }
